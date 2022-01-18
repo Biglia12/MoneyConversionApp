3a814e1de9d960cd819620a10b1ee5f38be59b33
Ezequiel Biglia <ebiglia@q4tech.com>
2022-01-18 20:49:13 -0300

    Initial commit

---
+++ app/src/main/java/com/kotlin/moneyconversionapp/activities/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.kotlin.moneyconversionapp.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.kotlin.moneyconversionapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+    }
+}
