f45698e4fb7cdf14919d542f30014380b0dd747c
Ezequiel Biglia <ebiglia@q4tech.com>
2022-01-18 20:56:47 -0300

    test comment

--- app/src/main/java/com/kotlin/moneyconversionapp/activities/MainActivity.kt
+++ app/src/main/java/com/kotlin/moneyconversionapp/activities/MainActivity.kt
@@ -3,7 +3,7 @@ package com.kotlin.moneyconversionapp.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.kotlin.moneyconversionapp.R
-
+//test
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
