4e61d459a2076ccc9ef7886fb9f3a6c4cac475f9
Ezequiel Biglia <ebiglia@q4tech.com>
2022-01-19 14:15:06 -0300

    se arregla bottom navigation

--- app/src/main/java/com/kotlin/moneyconversionapp/activities/MainActivity.kt
+++ app/src/main/java/com/kotlin/moneyconversionapp/activities/MainActivity.kt
@@ -2,14 +2,44 @@ package com.kotlin.moneyconversionapp.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import com.kotlin.moneyconversionapp.R
+import com.kotlin.moneyconversionapp.databinding.ActivityMainBinding
+import com.kotlin.moneyconversionapp.fragments.DashBoardFragment
+import com.kotlin.moneyconversionapp.fragments.HistoryFragment
+
 //test
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val dashBoardFragment = DashBoardFragment()
+    private val historyFragment = HistoryFragment()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        bottomNavigation()
 
+    }
 
+    private fun bottomNavigation() {
+        binding.navigationBottom.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.item_resume -> replaceFragment(dashBoardFragment)
+                R.id.item_history -> replaceFragment(historyFragment)
+            }
+            true
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.fragment_container, fragment)
+            commit()
+
+        }
     }
 }
