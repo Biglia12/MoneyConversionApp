bfb5663d8e7e6b4e22bffa5da615a405957bc211
Ezequiel Biglia <ezequielbiglia12@gmail.com>
2022-09-15 17:12:47 -0300

    sse agrega si hay conexion a internet

--- app/src/main/java/com/kotlin/moneyconversionapp/ui/view/activities/MainActivity.kt
+++ app/src/main/java/com/kotlin/moneyconversionapp/ui/view/activities/MainActivity.kt
@@ -1,14 +1,15 @@
 package com.kotlin.moneyconversionapp.ui.view.activities
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.activity.viewModels
+import android.view.View.GONE
+import android.view.View.VISIBLE
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import com.kotlin.moneyconversionapp.Application
 import com.kotlin.moneyconversionapp.R
 import com.kotlin.moneyconversionapp.databinding.ActivityMainBinding
 import com.kotlin.moneyconversionapp.ui.view.fragments.DashBoardFragment
 import com.kotlin.moneyconversionapp.ui.view.fragments.HistoryFragment
-import com.kotlin.moneyconversionapp.ui.viewmodel.DollarViewModel
 
 //test
 class MainActivity : AppCompatActivity() {
@@ -16,14 +17,29 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private val dashBoardFragment = DashBoardFragment()
     private val historyFragment = HistoryFragment()
+    private val application: Application = Application()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        bottomNavigation()
+        checkConecction()
 
+    }
+
+    private fun checkConecction() {
+        if (application.isConnected(this)) {
+            binding.constraintErrorInternet.visibility = GONE
+            binding.navigationBottom.visibility = VISIBLE
+            bottomNavigation()
+        } else {
+            binding.constraintErrorInternet.visibility = VISIBLE
+            binding.navigationBottom.visibility = GONE
+            binding.retryErrorButton.setOnClickListener {
+                checkConecction()
+            }
+        }
     }
 
     private fun bottomNavigation() {
