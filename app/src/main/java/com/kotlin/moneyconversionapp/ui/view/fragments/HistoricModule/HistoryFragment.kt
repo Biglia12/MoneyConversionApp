b843ab6ad37f6dc59d3a46e5fad2d6a71bf98534
Ezequiel Biglia <ezequielbiglia12@gmail.com>
2023-03-16 14:03:18 -0300

    se modifica tamaño vector

--- app/src/main/java/com/kotlin/moneyconversionapp/ui/view/fragments/HistoricModule/HistoryFragment.kt
+++ app/src/main/java/com/kotlin/moneyconversionapp/ui/view/fragments/HistoricModule/HistoryFragment.kt
@@ -1,10 +1,12 @@
 package com.kotlin.moneyconversionapp.ui.view.fragments.HistoricModule
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import com.kotlin.moneyconversionapp.databinding.FragmentHistoryBinding
@@ -36,6 +38,29 @@ class HistoryFragment : Fragment() {
 
         })*/
 
+    }
+
+
+    @SuppressLint("RestrictedApi")
+    override fun onStart() {
+        super.onStart()
+        (activity as AppCompatActivity).supportActionBar!!.setShowHideAnimationEnabled(false) // esto no permitira cuando el action bar se oculte haga una animacion
+        (activity as AppCompatActivity).supportActionBar!!.hide() // se oculta el action bar
+    }
+
+    @SuppressLint("RestrictedApi")
+    override fun onStop() {
+        super.onStop()
+        (activity as AppCompatActivity).supportActionBar!!.setShowHideAnimationEnabled(false)
+        (activity as AppCompatActivity).supportActionBar!!.show()
+    }
+
+
+    @SuppressLint("RestrictedApi")
+    override fun onResume() {
+        super.onResume()
+        (activity as AppCompatActivity).supportActionBar!!.setShowHideAnimationEnabled(false)
+        (activity as AppCompatActivity).supportActionBar!!.hide()
     }
 
 }
