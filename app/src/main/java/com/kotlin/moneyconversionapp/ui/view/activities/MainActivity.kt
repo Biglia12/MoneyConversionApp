b04a0c02d1f62acd2012b39b6d2344ae2a53c3d7
Biglia12 <ezequielbiglia12@gmail.com>
2023-05-21 22:11:51 -0300

    se paso logica al view model. solo mostrara el toast la view. el viewmodel se encargara de la loigca de la actualizacion. por ahora se hicieron prueba en playstore (falta seguir porbando para ver como funciona para sacar a produccion)

--- app/src/main/java/com/kotlin/moneyconversionapp/ui/view/activities/MainActivity.kt
+++ app/src/main/java/com/kotlin/moneyconversionapp/ui/view/activities/MainActivity.kt
@@ -1,38 +1,28 @@
 package com.kotlin.moneyconversionapp.ui.view.activities
 
 import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
-import android.util.Log
-import android.view.View
 import android.view.View.GONE
 import android.view.View.VISIBLE
 import android.widget.Toast
+import androidx.activity.viewModels
 import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.lifecycleScope
-import com.google.android.material.snackbar.Snackbar
+import androidx.lifecycle.Observer
 import com.google.android.play.core.appupdate.AppUpdateManager
 import com.google.android.play.core.appupdate.AppUpdateManagerFactory
-import com.google.android.play.core.install.InstallState
-import com.google.android.play.core.install.InstallStateUpdatedListener
 import com.google.android.play.core.install.model.AppUpdateType
-import com.google.android.play.core.install.model.InstallStatus
 import com.google.android.play.core.install.model.UpdateAvailability
 import com.google.android.play.core.ktx.isFlexibleUpdateAllowed
 import com.google.android.play.core.ktx.isImmediateUpdateAllowed
 import com.kotlin.moneyconversionapp.MoneyApplication
-import com.kotlin.moneyconversionapp.R
 import com.kotlin.moneyconversionapp.databinding.ActivityMainBinding
 import com.kotlin.moneyconversionapp.ui.view.fragments.CalculatorModule.CalculatorFragment
 import com.kotlin.moneyconversionapp.ui.view.fragments.DashBoardModule.DashBoardFragment
 import com.kotlin.moneyconversionapp.ui.view.fragments.HistoricModule.HistoryFragment
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
-import java.security.AccessController.getContext
-import kotlin.time.Duration.Companion.seconds
-import kotlin.time.seconds
+import com.kotlin.moneyconversionapp.ui.viewmodel.Main.MainViewModel
+import com.kotlin.moneyconversionapp.ui.viewmodel.Main.MainViewModelFactory
 
 
 //test
@@ -45,13 +35,11 @@ class MainActivity : AppCompatActivity() {
     private lateinit var currentFragment: Fragment
     private val moneyApplication: MoneyApplication = MoneyApplication()
 
-    /*private val mainViewModel: MainViewModel by viewModels {
+    private val mainViewModel: MainViewModel by viewModels {
         MainViewModelFactory(this)
-    }*/
-    private val MY_REQUEST_CODE: Int = 100
+    }
 
     private lateinit var appUpdateManager: AppUpdateManager
-    private val updateType = AppUpdateType.FLEXIBLE
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -64,67 +52,33 @@ class MainActivity : AppCompatActivity() {
 
         appUpdateManager = AppUpdateManagerFactory.create(applicationContext)
 
-        //mainViewModel.checkAppUpdate()
-
         checkConecction()
 
-        if (updateType == AppUpdateType.FLEXIBLE){
-            appUpdateManager.registerListener(installStateUpdateListener)
-        }
-
-        checkForAppUpdate()
-        // observeLiveData()
+        observableLive()
 
     }
 
-    private val installStateUpdateListener = InstallStateUpdatedListener { state ->
-        if (state.installStatus() == InstallStatus.DOWNLOADED) {
-            Toast.makeText(
-                this@MainActivity,
-                "Descarga exitosa. Reiniciando app en 5 segundos.",
-                Toast.LENGTH_SHORT
-            )
-                .show()
-            lifecycleScope.launch {
-                delay(5000)
-                appUpdateManager.completeUpdate()
-            }
-        }
-    }
+    private fun observableLive() {
 
-    private fun checkForAppUpdate() {
-        appUpdateManager.appUpdateInfo.addOnSuccessListener { info ->
-            val isUpdateAvailable = info.updateAvailability() == UpdateAvailability.UPDATE_AVAILABLE
-            val isUpdateAllowed = when (updateType) {
-                AppUpdateType.FLEXIBLE -> info.isFlexibleUpdateAllowed
-                AppUpdateType.IMMEDIATE -> info.isImmediateUpdateAllowed
-                else -> false
-            }
-            if (isUpdateAvailable) {
-                appUpdateManager.startUpdateFlowForResult(
-                    info,
-                    updateType,
-                    this,
-                    MY_REQUEST_CODE
-                )
-            }
-        }
-    }
+        mainViewModel.setAppUpdateManager(appUpdateManager)
+        mainViewModel.updateType()
+        mainViewModel.checkForAppUpdate(this)
 
+        mainViewModel.downloadSuccessLiveData.observe(this, Observer {isSuccess ->
+            if (isSuccess) {
+                Toast.makeText(
+                    applicationContext,
+                    "Descarga exitosa. Reiniciando app en breve.",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        })
+
+
+    }
     override fun onResume() {
         super.onResume()
-        if (updateType == AppUpdateType.IMMEDIATE) {
-            appUpdateManager.appUpdateInfo.addOnSuccessListener { info ->
-                if (info.updateAvailability() == UpdateAvailability.DEVELOPER_TRIGGERED_UPDATE_IN_PROGRESS) {
-                    appUpdateManager.startUpdateFlowForResult(
-                        info,
-                        updateType,
-                        this,
-                        MY_REQUEST_CODE
-                    )
-                }
-            }
-        }
+        mainViewModel.onResume(this)
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
@@ -136,110 +90,10 @@ class MainActivity : AppCompatActivity() {
 
     override fun onDestroy() {
         super.onDestroy()
-        if (updateType == AppUpdateType.FLEXIBLE){
-            appUpdateManager.unregisterListener(installStateUpdateListener)
-        }
+        mainViewModel.onDestroy()
     }
 
-    /*
-        private fun checkForAppUpdate() {
 
-            val appUpdateInfoTask = appUpdateManager.appUpdateInfo
-
-    // Checks that the platform will allow the specified type of update.
-            appUpdateInfoTask.addOnSuccessListener { appUpdateInfo ->
-                if (appUpdateInfo.updateAvailability() == UpdateAvailability.UPDATE_AVAILABLE
-                    // This example applies an immediate update. To apply a flexible update
-                    // instead, pass in AppUpdateType.FLEXIBLE
-                    && appUpdateInfo.isUpdateTypeAllowed(AppUpdateType.FLEXIBLE)
-                ) {
-                    // Request the update.
-                    appUpdateManager.startUpdateFlowForResult(appUpdateInfo, AppUpdateType.FLEXIBLE, this, MY_REQUEST_CODE)
-                }
-            }
-
-            // Before starting an update, register a listener for updates.
-            appUpdateManager.registerListener(listener)
-        }
-
-
-        val listener = { state: InstallState ->
-            if (state.installStatus() == InstallStatus.DOWNLOADED) {
-                 popupSnackbarForCompleteUpdate()
-            }
-        }
-
-        private fun popupSnackbarForCompleteUpdate() {
-            val snackbar = Snackbar.make(
-                findViewById(R.id.main_activity),
-                "An update has just been downloaded.",
-                Snackbar.LENGTH_INDEFINITE
-            )
-            snackbar.setAction("INSTALL") { view: View? -> appUpdateManager.completeUpdate() }
-            snackbar.setActionTextColor(
-                resources.getColor(com.kotlin.moneyconversionapp.R.color.colorPrimary)
-            )
-            snackbar.show()
-        }
-
-        override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-            super.onActivityResult(requestCode, resultCode, data)
-            if (requestCode == MY_REQUEST_CODE) {
-                if (resultCode != RESULT_OK) {
-                    Log.e("MY_APP", "Update flow failed! Result code: $resultCode")
-                }
-            }
-        }
-
-        override fun onStop() {
-            super.onStop()
-            appUpdateManager.unregisterListener(listener)
-        }
-
-        override fun onResume() {
-            super.onResume()
-            appUpdateManager
-                .appUpdateInfo
-                .addOnSuccessListener { appUpdateInfo ->
-                    if (appUpdateInfo.installStatus() == InstallStatus.DOWNLOADED) {
-                        popupSnackbarForCompleteUpdate()
-                    }
-                }
-        }
-    */
-
-    /* private fun observeLiveData() {
-             mainViewModel.showSnackbarEvent.observe(this) {
-                 showSnackbar()
-             }
-         }
-
-         private fun showSnackbar() {
-             Snackbar.make(
-                 findViewById(R.id.main_activity),
-                 "An update has just been downloaded.",
-                 Snackbar.LENGTH_INDEFINITE
-             ).apply {
-                 setAction("INSTALAR") { mainViewModel.appUpdateManager.completeUpdate() }
-                 setActionTextColor(resources.getColor(R.color.colorPrimary))
-                 show()
-             }
-         }
-
-         override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-             super.onActivityResult(requestCode, resultCode, data)
-             mainViewModel.onActivityResult(requestCode, resultCode, data)
-         }
-
-         override fun onStop() {
-             super.onStop()
-             mainViewModel.onStop()
-         }
-
-         override fun onResume() {
-             super.onResume()
-             mainViewModel.onResume()
-         }*/
 
     private fun checkConecction() {
         if (moneyApplication.isConnected(this)) {
--- app/src/main/java/com/kotlin/moneyconversionapp/ui/viewmodel/Main/MainViewModel.kt
+++ app/src/main/java/com/kotlin/moneyconversionapp/ui/viewmodel/Main/MainViewModel.kt
@@ -1,112 +1,90 @@
 package com.kotlin.moneyconversionapp.ui.viewmodel.Main
 
 import android.app.Activity
-import android.app.Activity.RESULT_OK
-import android.content.Intent
-import android.util.Log
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.google.android.play.core.appupdate.AppUpdateManagerFactory
-import com.google.android.play.core.install.InstallState
+import androidx.lifecycle.viewModelScope
+import com.google.android.play.core.appupdate.AppUpdateInfo
+import com.google.android.play.core.appupdate.AppUpdateManager
+import com.google.android.play.core.install.InstallStateUpdatedListener
 import com.google.android.play.core.install.model.AppUpdateType
 import com.google.android.play.core.install.model.InstallStatus
 import com.google.android.play.core.install.model.UpdateAvailability
-import kotlin.math.log
+import com.google.android.play.core.ktx.isFlexibleUpdateAllowed
+import com.google.android.play.core.ktx.isImmediateUpdateAllowed
+import com.kotlin.moneyconversionapp.ui.view.activities.MainActivity
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainViewModel(val activity: Activity) : ViewModel() {
 
-    val appUpdateManager = AppUpdateManagerFactory.create(activity)
+    val checkForAppUpdateLiveData = MutableLiveData<AppUpdateInfo?>()
+    private val updateType = AppUpdateType.FLEXIBLE
+    private lateinit var appUpdateManager: AppUpdateManager
+    var downloadToast  = MutableLiveData<String>()
     private val MY_REQUEST_CODE: Int = 100
+    val downloadSuccessLiveData = MutableLiveData<Boolean>()
 
-    // Returns an intent object that you use to check for an update.
-    val appUpdateInfoTask = appUpdateManager.appUpdateInfo
+  init {
 
-    private val _showSnackbarEvent = MutableLiveData<Unit>()
-    val showSnackbarEvent: LiveData<Unit>
-        get() = _showSnackbarEvent
+  }
 
-    init {
-
-        checkAppUpdate()
-
+    fun setAppUpdateManager(manager: AppUpdateManager) {
+        appUpdateManager = manager
     }
 
-    private val listener = { state: InstallState ->
-            if (state.installStatus() == InstallStatus.DOWNLOADED) {
-                // After the update is downloaded, show a notification
-                // and request user confirmation to restart the app.
-                _showSnackbarEvent.value = Unit
+    private val installStateUpdateListener = InstallStateUpdatedListener { state ->
+        if (state.installStatus() == InstallStatus.DOWNLOADED) {
+            //downloadToast.value = "Descarga exitosa. Reiniciando app en 5 segundos."
+            viewModelScope.launch {
+                delay(5000)
+                appUpdateManager.completeUpdate()
+                downloadSuccessLiveData.postValue(true)
             }
+        }
+    }
+    fun updateType(){
+        if (updateType == AppUpdateType.FLEXIBLE){
+            appUpdateManager.registerListener(installStateUpdateListener)
+        }
+    }
+    fun checkForAppUpdate(mainActivity: MainActivity) {
+        appUpdateManager.appUpdateInfo.addOnSuccessListener { info ->
+            val isUpdateAvailable = info.updateAvailability() == UpdateAvailability.UPDATE_AVAILABLE
+            val isUpdateAllowed = when (updateType) {
+                AppUpdateType.FLEXIBLE -> info.isFlexibleUpdateAllowed
+                AppUpdateType.IMMEDIATE -> info.isImmediateUpdateAllowed
+                else -> false
+            }
+            if (isUpdateAvailable) {
+                appUpdateManager.startUpdateFlowForResult(
+                    info,
+                    updateType,
+                    mainActivity,
+                    MY_REQUEST_CODE
+                )
+            }
+        }
     }
 
-
-    fun checkAppUpdate() {
-        try {
-            // Checks that the platform will allow the specified type of update.
-            appUpdateInfoTask.addOnSuccessListener { appUpdateInfo ->
-                if (appUpdateInfo.updateAvailability() == UpdateAvailability.UPDATE_AVAILABLE
-                    // This example applies an immediate update. To apply a flexible update
-                    // instead, pass in AppUpdateType.FLEXIBLE
-                    && appUpdateInfo.isUpdateTypeAllowed(AppUpdateType.FLEXIBLE)
-                ) {
-                    // Request the update.
+    fun onResume(mainActivity: MainActivity) {
+        if (updateType == AppUpdateType.IMMEDIATE) {
+            appUpdateManager.appUpdateInfo.addOnSuccessListener { info ->
+                if (info.updateAvailability() == UpdateAvailability.DEVELOPER_TRIGGERED_UPDATE_IN_PROGRESS) {
                     appUpdateManager.startUpdateFlowForResult(
-                        // Pass the intent that is returned by 'getAppUpdateInfo()'.
-                        appUpdateInfo,
-                        // Or 'AppUpdateType.FLEXIBLE' for flexible updates.
-                        AppUpdateType.FLEXIBLE,
-                        // The current activity making the update request.
-                        activity,
-                        // Include a request code to later monitor this update request.
+                        info,
+                        updateType,
+                        mainActivity,
                         MY_REQUEST_CODE
                     )
                 }
             }
-
-            // Before starting an update, register a listener for updates.
-            appUpdateManager.registerListener(listener)
-        }catch (e: Exception) {
-            e.printStackTrace()
-            Log.e("Error app update checkUpdate", e.toString())
-        }
-
-    }
-
-
-    fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        try {
-            if (requestCode == MY_REQUEST_CODE) {
-                if (resultCode != RESULT_OK) {
-                    Log.e("MY_APP", "Update flow failed! Result code: $resultCode")
-                    // If the update is cancelled or fails,
-                    // you can request to start the update again.
-                }
-            }
-        } catch (e: Exception) {
-            e.printStackTrace()
-            Log.e("Error app update onactivityResule", e.toString())
         }
     }
 
-    fun onStop (){
-       appUpdateManager.unregisterListener(listener)
-    }
-
-    fun onResume(){
-        try {
-            appUpdateManager
-                .appUpdateInfo
-                .addOnSuccessListener { appUpdateInfo ->
-                    // If the update is downloaded but not installed,
-                    // notify the user to complete the update.
-                    if (appUpdateInfo.installStatus() == InstallStatus.DOWNLOADED) {
-                        _showSnackbarEvent.value = Unit
-                    }
-                }
-        }catch (e: Exception) {
-            e.printStackTrace()
-            Log.e("Error app update onResume", e.toString())
+     fun onDestroy() {
+        if (updateType == AppUpdateType.FLEXIBLE){
+            appUpdateManager.unregisterListener(installStateUpdateListener)
         }
     }
 
